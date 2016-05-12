214641f85d25b5476d478f2ba559c6166edc5138
Rank <sojitradhawal@gmail.com>
2016-05-12 00:23:22 -0400

    v01

---
+++ restoapi.js
@@ -0,0 +1,22 @@
+var pg = require('pg');
+var express = require('express');
+var app = express();
+var conString = "postgres://oponrmwidypexs:_vpbC0gM1LgSNYjrbM0t3zswd3@ec2-107-22-235-119.compute-1.amazonaws.com:5432/dclelckuq7us2q?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory";
+
+
+app.get('/', function(request, response, next) {
+var id = request.params.id;
+var client = new pg.Client(conString);
+client.connect(function(err) {
+  if(err) {
+    return console.error('could not connect to postgres', err);
+  }
+  client.query('SELECT restaurant.stime, restaurant.etime FROM restaurant WHERE restaurant.id=id', function(err, result) {
+    if(err) {
+      return console.error('error running query', err);
+    }
+    console.log(result.rows[0]);
+    client.end();
+  });
+});
+  }); 
